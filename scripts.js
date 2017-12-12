71953a500f740a4a8bc5af1e5ebc597ba3660fa2
thismax <max.g.jacobs@gmail.com>
2017-12-12 11:23:07 -0800

    added psuedocode

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-// Using namesList and addressesList, accomplish the objective listed in README.md
+// import lodash, names, addresses
+const _ = require('lodash');
+const names = require('./names');
+const addresses = require('./addresses');
+
+const combineNamesAndAddresses = (namesList, addressesList) => {
+  // initialize namesAndAddresses array
+  // iterate through namesList
+  // find object in addressesList that matches current namesList object id property
+  // extend current names object with matching address object
+  // log/return namesAndAdresses
+};
+
+// call combineNamesAndAddresses
